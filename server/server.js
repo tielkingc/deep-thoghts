ba842b29d91422abe17cbb6bde8ce9e9e9a3f7ba
Christian Tielking <tielking5@gmail.com>
2021-08-24 18:34:52 -0400

    lesson 1

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const db = require('./config/connection');
 
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+async function startApolloServer(typeDefs, resolvers) {
+  await server.start();
+
+  await server.applyMiddleware({
+    app,
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +28,6 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
